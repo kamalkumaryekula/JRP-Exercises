729b04d956fd7c1f71b239a0dba75150c59ac987
kamalkumaryekula <krishnakamalyekula2001@gmail.com>
2026-04-09 12:47:30 +0530

    g

--- welcome to python/brain_teasers.py
+++ welcome to python/brain_teasers.py
@@ -3,8 +3,19 @@
 print(1450 / 60 / 3)
 
 # 2. ID check : will this be true or false? why?
-x = 257
-y = 257
-print(id(x))
-print(id(y))
+x = 1000
+y = 1000
+
 print(x is y)
+
+# 3.Input Math: If you enter 10 and then 20, what happens?
+x = input("Enter a number: ")
+y = input("Enter another number: ")
+print(x + y)
+
+# 4. Float Identity: True or False? Think about pooling.
+x = 2.3
+y = 2.3
+
+print(x is y)
+
